37837dc00c6efd518ce4946917d87f5ccd7b0cab
VioletKitty23 <115854301+VioletKitty23@users.noreply.github.com>
2024-04-15 20:01:15 +0000

    Added a constructor, the beginnings of a copy constructor, and a print function

--- linkedListImp.cpp
+++ linkedListImp.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include "linkedList.h"
+using namespace std;
 
+/*
+    Constructor function definitions
+*/
+linkedList::linkedList()
+{
+    //With no elements yet, the pointers are null pointers, and the element counter is zero
+    head = nullptr;
+    tail = nullptr;
+    count = 0;
+}
+
+linkedList::linkedList(const linkedList &x)
+{
+    if(x.count == 0){ //If the list being copied has no elements, it is functionally the same as the default constructor
+        head = nullptr;
+        tail = nullptr;
+        count = 0;
+    }
+}
+
+/*
+    Accessor
+*/
+void linkedList::print()
+{
+    node *current; //Creates a pointer to a node
+    current = head; //Assigns the new pointer to the same as the head, so that the head isn't modified while iterating through the list
+
+    while(current != nullptr){ //Will run until the "current" pointer is a null pointer, meaning it reached the end of the list
+        cout << current -> num << " -> "; //Outputs the list element
+        current = current->next; //Updates the pointer to the next element in the list
+    }
+
+    cout << endl;
+}
